c9fd85a88f64baac2e31593e2504f71297b2120c
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-03-08 15:00:00 +0530

    CLI commands created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Add { file: String },
+    Commit { message: String },
+    Log,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Init => println!("Initializing repository"),
+        Commands::Add { file } => println!("Adding file {}", file),
+        Commands::Commit { message } => println!("Commit message: {}", message),
+        Commands::Log => println!("Showing commit log"),
+    }
 }
